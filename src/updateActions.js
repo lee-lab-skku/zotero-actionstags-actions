f6ab316821d2b02a71dd7134be0466ba12d78668
Shin Taeha <mirinae3145@g.skku.edu>
2025-12-01 13:17:30 +0900

    avoid problem...

--- src/updateActions.js
+++ src/updateActions.js
@@ -6,11 +6,11 @@ const latest = await fetch(TARGET_URL).then(res => res.json());
 const latestVersion = latest.tag_name;
 const prevVersion = Zotero.Prefs.get(PREF_KEY);
 
-if (prevVersion === latestVersion) {
-    if (triggerType === 'menu')
-        return 'Actions are already up to date.';
+if (!prevVersion)
+    return 'Automatic update is now set.';
+if (prevVersion === latestVersion)
     return;
-}
+
 Zotero.Prefs.set(PREF_KEY, latestVersion);
 
 const assetUrl = latest.assets.find(asset => asset.name.endsWith('.yml')).browser_download_url;

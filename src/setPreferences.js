73e8884b746dc22b235ad28f0533746b196624ed
Shin Taeha <mirinae3145@g.skku.edu>
2025-12-01 17:37:14 +0900

    remove coded return

--- src/setPreferences.js
+++ src/setPreferences.js
@@ -11,9 +11,8 @@ let groupID = Zotero.Prefs.get(PREF_GROUP_KEY);
 if (!groupID) {
     const groups = Zotero.Groups.getAll();
     ok = await Services.prompt.select(null, 'Organization', 'Which group is your organization?', groups.map(g => g.name), selected);
-    if (!ok) {
-        return 1;
-    }
+    if (!ok)
+        return;
     groupID = groups[selected.value].id;
     Zotero.Prefs.set(PREF_GROUP_KEY, groupID);
     anySet = true;
@@ -24,9 +23,8 @@ let reviewCollectionKey = Zotero.Prefs.get(PREF_REVIEW_COLLECTION_KEY);
 if (!reviewCollectionKey) {
     const cols = Zotero.Collections.getByLibrary(targetLibraryID);
     ok = await Services.prompt.select(null, 'Review', 'Which collection is for review?', cols.map(c => c.name), selected);
-    if (!ok) {
-        return 1;
-    }
+    if (!ok)
+        return;
     reviewCollectionKey = cols[selected.value].key;
     Zotero.Prefs.set(PREF_REVIEW_COLLECTION_KEY, reviewCollectionKey);
     anySet = true;
@@ -35,9 +33,8 @@ let shareCollectionKey = Zotero.Prefs.get(PREF_SHARE_COLLECTION_KEY);
 if (!shareCollectionKey) {
     const cols = Zotero.Collections.getByLibrary(targetLibraryID);
     ok = await Services.prompt.select(null, 'Share', 'Which collection is for sharing?', cols.map(c => c.name), selected);
-    if (!ok) {
-        return 1;
-    }
+    if (!ok)
+        return;
     shareCollectionKey = cols[selected.value].key;
     Zotero.Prefs.set(PREF_SHARE_COLLECTION_KEY, shareCollectionKey);
     anySet = true;
@@ -46,9 +43,8 @@ if (!shareCollectionKey) {
 let reviewerName = Zotero.Prefs.get(PREF_NAME);
 if (!reviewerName) {
     ok = await Services.prompt.prompt(null, 'Name', 'What is your name? (In Korean, no space.)', selected, null, {});
-    if (!ok) {
-        return 1;
-    }
+    if (!ok)
+        return;
     reviewerName = selected.value;
     Zotero.Prefs.set(PREF_NAME, reviewerName);
 }

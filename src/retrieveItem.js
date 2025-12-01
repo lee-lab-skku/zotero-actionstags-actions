0fefd309d27f86f41d9124b0c5ff6ed10e2c134e
Shin Taeha <mirinae3145@g.skku.edu>
2025-12-01 11:18:12 +0900

    remove all async keywords

--- src/retrieveItem.js
+++ src/retrieveItem.js
@@ -1,59 +1,57 @@
-(async () => {
-    const oldItem = Zotero.Items.get(item.id);
+const oldItem = Zotero.Items.get(item.id);
 
-    if (Zotero.ActionsTags.__retrieveItemRunning) return;
-    Zotero.ActionsTags.__retrieveItemRunning = true;
-    
-    const cols = Zotero.Collections.getByLibrary(1);
-    const selected = new Object();
-    const ok = await Services.prompt.select(null, 'Selection', 'Select the collection to move the item to.', cols.map(c => c.name), selected);
+if (Zotero.ActionsTags.__retrieveItemRunning) return;
+Zotero.ActionsTags.__retrieveItemRunning = true;
 
-    if (!ok) {
-        Zotero.ActionsTags.__retrieveItemRunning = false;
-        return 1;
-    }
-    const coll = cols[selected.value];
+const cols = Zotero.Collections.getByLibrary(1);
+const selected = new Object();
+const ok = await Services.prompt.select(null, 'Selection', 'Select the collection to move the item to.', cols.map(c => c.name), selected);
 
-    const type = oldItem.itemTypeID;
-    const newItem = new Zotero.Item(type);
-    newItem.libraryID = 1;
+if (!ok) {
+    Zotero.ActionsTags.__retrieveItemRunning = false;
+    return 1;
+}
+const coll = cols[selected.value];
 
-    const fieldIDs = Zotero.ItemFields.getItemTypeFields(type);
-    for (const fieldID of fieldIDs) {
-        const fieldName = Zotero.ItemFields.getName(fieldID);
-        if (fieldName === 'key' || fieldName === 'version' || fieldName === 'libraryID')
-            continue;
+const type = oldItem.itemTypeID;
+const newItem = new Zotero.Item(type);
+newItem.libraryID = 1;
 
-        const value = oldItem.getField(fieldName);
-        if (!!value)
-            newItem.setField(fieldName, value);
-    }
+const fieldIDs = Zotero.ItemFields.getItemTypeFields(type);
+for (const fieldID of fieldIDs) {
+    const fieldName = Zotero.ItemFields.getName(fieldID);
+    if (fieldName === 'key' || fieldName === 'version' || fieldName === 'libraryID')
+        continue;
 
-    const creators = oldItem.getCreators();
-    if (!!creators)
-        newItem.setCreators(creators);
-    if (!!coll)
-        newItem.addToCollection(coll.id);
+    const value = oldItem.getField(fieldName);
+    if (!!value)
+        newItem.setField(fieldName, value);
+}
 
-    await newItem.saveTx();
+const creators = oldItem.getCreators();
+if (!!creators)
+    newItem.setCreators(creators);
+if (!!coll)
+    newItem.addToCollection(coll.id);
 
-    const attachmentIDs = oldItem.getAttachments();
-    if (attachmentIDs.length) {
-        for (const attachmentID of attachmentIDs) {
-            const oldAtt = Zotero.Items.get(attachmentID);
-            if (oldAtt.isAttachment()) {
-                const path = Zotero.File.pathToFile(oldAtt.getFilePath());
-                if (!!path) {
-                    await Zotero.Attachments.importFromFile({
-                        file: path, parentItemID: newItem.id, libraryID: 1
-                    });
-                }
+await newItem.saveTx();
+
+const attachmentIDs = oldItem.getAttachments();
+if (attachmentIDs.length) {
+    for (const attachmentID of attachmentIDs) {
+        const oldAtt = Zotero.Items.get(attachmentID);
+        if (oldAtt.isAttachment()) {
+            const path = Zotero.File.pathToFile(oldAtt.getFilePath());
+            if (!!path) {
+                await Zotero.Attachments.importFromFile({
+                    file: path, parentItemID: newItem.id, libraryID: 1
+                });
             }
         }
     }
+}
 
-    await Zotero.Items.erase([oldItem.id]);
+await Zotero.Items.erase([oldItem.id]);
 
-    Zotero.ActionsTags.__retrieveItemRunning = false;
-    return 0;
-})();
+Zotero.ActionsTags.__retrieveItemRunning = false;
+return 0;

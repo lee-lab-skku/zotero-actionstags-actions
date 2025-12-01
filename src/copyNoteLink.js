dc641467b0a3d08d79a009be8fc738fb943d6829
Shin Taeha <mirinae3145@g.skku.edu>
2025-12-01 16:55:25 +0900

    fix: early return structure

--- src/copyNoteLink.js
+++ src/copyNoteLink.js
@@ -1,8 +1,9 @@
-if (item.isNote() && Zotero.BetterNotes)
-    uri = Zotero.BetterNotes.api.convert.note2link(item);
-else
-    return "The selected item is not a note, or Better Notes plugin is not detected."
+if (!item.isNote())
+    return "The selected item is not a note.";
+if (!Zotero.BetterNotes)
+    return "Better Notes plugin is not detected.";
 
+const uri = Zotero.BetterNotes.api.convert.note2link(item);
 const text = `${item.getField('title')} (${Zotero.Items.getTopLevel([item])[0].getField("citationKey")})`;
 
 const clipboard = new Zotero.ActionsTags.api.utils.ClipboardHelper();

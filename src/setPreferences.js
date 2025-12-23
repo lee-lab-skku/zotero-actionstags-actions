65b50df75e42f5105f53c95620b6861fb4e1aeaa
Shin Taeha <mirinae3145@g.skku.edu>
2025-12-23 10:09:01 +0900

    tara id compatible

--- src/setPreferences.js
+++ src/setPreferences.js
@@ -1,4 +1,5 @@
 const PREF_GROUP_KEY = 'actionsTags.actions.groupID';
+const PREF_GROUP_KEY_ALT = 'tara.groupID';
 const PREF_REVIEW_COLLECTION_KEY = 'actionsTags.actions.reviewCollectionKey';
 const PREF_SHARE_COLLECTION_KEY = 'actionsTags.actions.shareCollectionKey';
 const PREF_NAME = 'actionsTags.actions.reviewerName';
@@ -9,13 +10,16 @@ let anySet = false;
 
 let groupID = Zotero.Prefs.get(PREF_GROUP_KEY);
 if (!groupID) {
-    const groups = Zotero.Groups.getAll();
-    ok = await Services.prompt.select(null, 'Organization', 'Which group is your organization?', groups.map(g => g.name), selected);
-    if (!ok)
-        return;
-    groupID = groups[selected.value].id;
-    Zotero.Prefs.set(PREF_GROUP_KEY, groupID);
-    anySet = true;
+    let groupID = Zotero.Prefs.get(PREF_GROUP_KEY_ALT);
+    if (!groupID) {
+        const groups = Zotero.Groups.getAll();
+        ok = await Services.prompt.select(null, 'Organization', 'Which group is your organization?', groups.map(g => g.name), selected);
+        if (!ok)
+            return;
+        groupID = groups[selected.value].id;
+        Zotero.Prefs.set(PREF_GROUP_KEY, groupID);
+        anySet = true;
+    }
 }
 const targetLibraryID = Zotero.Groups.getLibraryIDFromGroupID(groupID);
 

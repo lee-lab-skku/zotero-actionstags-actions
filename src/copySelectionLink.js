90e7f1d439d4a8c5dcca998f179472f15127a24c
Shin Taeha <mirinae3145@g.skku.edu>
2026-01-07 19:41:17 +0900

    fix: plain text

--- src/copySelectionLink.js
+++ src/copySelectionLink.js
@@ -20,13 +20,16 @@ function parseURI(item) {
 
     const uri = [baseURI, groupURI, collectionURI, itemURI].filter(uri => uri).join("/");
     const text = `${item.getField("citationKey")}`;
+    const html = `<a href="${uri}">${text}</a>`;
 
-    return `<a href="${uri}">${text}</a>`;
+    return { uri, html };
 }
 
-const links = items.map(item => item.isAttachment() ? item.parentItem : item).map(parseURI).join("<br>");
+const links = items.map(item => item.isAttachment() ? item.parentItem : item).map(parseURI);
 const clipboard = new Zotero.ActionsTags.api.utils.ClipboardHelper();
-clipboard.addText(links, "text/html");
+
+clipboard.addText(links.map(link => link.uri).join("\n"), "text/unicode");
+clipboard.addText(links.map(link => link.html).join("<br>"), "text/html");
 clipboard.copy();
 
 return "Copied Zotero selection link to clipboard.";
